97bad8077d1849301388a877d5c63f6759f7c324
Morten Nissen <mail@hjemmesidekongen.dk>
2019-12-09 11:39:58 +0100

    Showcases

--- web/themes/custom/custom_theme/src/javascripts/app.js
+++ web/themes/custom/custom_theme/src/javascripts/app.js
@@ -16,7 +16,25 @@ jQuery(function ($) {
   var slider = tns({
     container: '.showcases',
     items: 1,
-    slideBy: 'page',
-    autoplay: true
+    autoplay: true,
+    autoplayHoverPause: true
   });
+
+  var nextShowcaseButtons = document.querySelectorAll('.js-show-next-showcase');
+  for(var nextInt = 0; nextInt < nextShowcaseButtons.length; nextInt++) {
+    var nextShowcase = nextShowcaseButtons[nextInt];
+
+    nextShowcase.addEventListener('click', function() {
+      slider.goTo('next');
+    });
+  }
+
+  var previousShowcaseButtons = document.querySelectorAll('.js-show-previous-showcase');
+  for(var prevInt = 0; prevInt < previousShowcaseButtons.length; prevInt++) {
+    var previousShowcase = previousShowcaseButtons[prevInt];
+
+    previousShowcase.addEventListener('click', function() {
+      slider.goTo('prev');
+    });
+  }
 });
